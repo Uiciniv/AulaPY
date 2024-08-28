269cd994fdaa1b5a2aaf0da8383c9cca12ba9e8f
Uiciniv <v.santos111@outlook.com>
2024-08-27 22:15:31 -0300

    Criação da função

---
+++ FuncaoExe.py
@@ -0,0 +1,14 @@
+def saudacao():
+    nome = input("Escreva seu nome")
+    print(f"Salve {nome}")
+saudacao()
+
+def soma (n1, n2):
+    return str(n1 + n2)
+
+a= int(input("Insira qualquer numero: "))
+b= int(input("Insira qualquer numero: "))
+
+
+print(soma(a,b))
+
